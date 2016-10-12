c1617ecdb508d5c8c69ba79acc94fe58bbfabf04
André Taiar <andre.taiar@gmail.com>
2016-10-12 19:42:34 -0300

    Inserting controller.

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'molecules#index'
+
+  resource :molecules, only: [:index, :search] do
+    member do
+      get :index
+      get :search
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
